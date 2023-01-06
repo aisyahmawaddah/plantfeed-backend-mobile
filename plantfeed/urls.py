e3554c8a1cce5d3568d6a75e587548987867890d
ainsalleh1 <ainsalleh.01@gmail.com>
2023-01-07 02:28:55 +0800

    member and sharing updated

---
+++ plantfeed/urls.py
@@ -0,0 +1,16 @@
+from member import views
+from sharing import views
+from group import views
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', views.index ),
+    path('', include('member.urls')),
+    path('sharing/', include('sharing.urls')),
+    path('group/', include('group.urls')),
+
+]
