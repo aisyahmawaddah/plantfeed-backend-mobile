6e49182238d1624751cff8c735ed505cff30d55e
A'isyah Mawaddah <aisyahmawaddah@graduate.utm.my>
2025-10-29 19:49:15 +0800

    Remove nested repository folder

--- plantfeed-backend-mobile/README.md
+++
@@ -1,3 +0,0 @@
-# PlantFeedWeb
-
-test
--- plantfeed-backend-mobile/group/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Group_tbl
-
-
-class GroupForm(forms.ModelForm):
-    class Meta:
-        model = Group_tbl
-        fields = ('Name', 'About', 'Media')
